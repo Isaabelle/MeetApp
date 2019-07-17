b8a1d27d7857d01512501a2e4b3b523dbf26101a
Isabelle <tavaresisaabelle@gmail.com>
2019-07-17 17:06:40 -0300

    MeetApp

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'meetapp_database',
+  password: 'desafio02',
+  database: 'meetapp_database',
+  port: '5433',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
